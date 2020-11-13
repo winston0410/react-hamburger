cf2ac1b833c4952b9b28f48b44eea82ca07815e0
Hugo <>
2020-11-13 10:27:59 +0800

    :sparkles: Add children prop in PluMinus component

--- src/lib/PlusMinus.js
+++ src/lib/PlusMinus.js
@@ -2,16 +2,24 @@
 
 import React from 'react'
 
+// <button aria-expanded={isExpanded} aria-label="Expand button" type="button" {...attr}>
+//     <div className={`${isExpanded ? minusClass : plusClass} ${innerClass}`}></div>
+// </button>
+
 const PlusMinus = ({ isExpanded, innerClass, plusClass, minusClass, children, ...attr }) => {
   return (
     <button aria-expanded={isExpanded} aria-label="Expand button" type="button" {...attr}>
-        <div className={`${isExpanded ? minusClass : plusClass} ${innerClass}`}></div>
+        <span className={`${isExpanded ? minusClass : plusClass} ${innerClass}`}>
+          {children}
+        </span>
     </button>
   )
 }
 
 export default PlusMinus
 
-// <button className="container" aria-label="Expand button" type="button" {...attr}>
-//   <div className="button">+ and -</div>
+// <button aria-expanded={isExpanded} aria-label="Expand button" type="button" {...attr}>
+//     <div>
+//       {isExpanded ? '-' : '+'}
+//     </div>
 // </button>
